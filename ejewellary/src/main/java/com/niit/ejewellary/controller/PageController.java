3f9f5e21c45448d26ea5fe8698bdbdca19037755
sandeepmisir <sandeepmisir@gmail.com>
2017-03-09 16:21:27 +0530

    successfully loaded static resources

---
+++ ejewellary/src/main/java/com/niit/ejewellary/controller/PageController.java
@@ -0,0 +1,26 @@
+package com.niit.ejewellary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index() {
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting","welcome to spring web MVC");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/test")
+	public ModelAndView test(@RequestParam("greeting") String greeting) {
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}
+	
+	
+}
